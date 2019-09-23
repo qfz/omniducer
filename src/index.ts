a3c9e564b9de4f7a3a6af434af9128c6fa054741
Feng <feng@Fengs-MBP.home.gateway>
2019-09-24 00:33:09 +1000

    Initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-export const omniducer = () => {
-  throw new Error("not implemented")
+import {createAction, Action} from "redux-actions"
+
+const prefix = "Omni/"
+
+export const makeAction: (...args: Parameters<typeof createAction>) => ReturnType<typeof createAction> = (type, ...rest) => {
+  return createAction(`${prefix}${type}`, ...rest)
+}
+
+export const makeSelector = <AppState extends {[i: string]: any}>(actionCreator: typeof makeAction) => (appState: AppState) => {
+  return appState[actionCreator.toString()]
+}
+
+export const omniducer = <State, Payload>(state: State, action: Action<Payload>): State => {
+  if (action.type.startsWith(prefix)) {
+    return {
+      ...state,
+      [action.type]: action.payload
+    }
+  } else {
+    return state
+  }
 }
